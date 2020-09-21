a248bd8eba4120a06fa1dcdbcde2beb094a8eae9
Abhi4t7 <abhikore99@gmail.com>
2020-09-21 15:36:13 +0530

    modified function.py

--- function.py
+++ function.py
@@ -12,9 +12,17 @@ def printf(x):
         for y in x:
          print(y)
 
-a,b = [int(x) for x in input("Enter two value: ").split()]
-add(a,b)
-print("List is\n")
-x = list(map(int, input("Enter a multiple value: ").split()))
-#y = tuple(map(int, input("Enter a multiple value: ").split()))
+#a,b = [int(x) for x in input("Enter two value: ").split()]
+#add(a,b)
+x = list(map(int, input("Enter a multiple value for list: ").split()))
+y = tuple(map(int, input("Enter a multiple value for tuple: ").split()))
+print("List is")
 printf(x)
+print("Tuple is")
+print(y)
+print("END\n")
+print(x,y)
+x[0]=100
+#y[0]=100
+print(x,y)
+

6bd35f8e5b74d0ca203893cd557851a7425b7f14
Abhi4t7 <abhikore99.com>
2020-10-04 19:51:52 +0530

    modified dict

--- dict.py
+++ dict.py
@@ -1,7 +1,9 @@
+x=[int(x) for x in input("Enter number").split()]
 d={1:"al",2:"bj",3:"ll"}
 print(d)
-for i in d:
-    d[i]="Abhijit" + "ABh"
+#for i in d:
+ #   d[i]="Abhijit" + "ABh"
+d[1]=x
 print(d)
-print(d.keys())
-print(d.values())
+#print(d.keys())
+#print(d.values())

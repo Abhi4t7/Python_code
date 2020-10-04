e763d826e231d0c17f8a228292102f3c53614f59
Abhi4t7 <abhikore99.com>
2020-10-04 19:33:17 +0530

    modify

--- usr_input.py
+++ usr_input.py
@@ -1,6 +1,10 @@
-name=input("Enter name")
-id =int(input("Enter id"))
-number=int(input("Enter mobile number"))
-print(name)
-print(id)
-print(number)
+#name=input("Enter name")
+#id =int(input("Enter id"))
+#number=int(input("Enter mobile number"))
+#print(name)
+#print(id)
+#print(number)
+x,y,d=input("Enter 3 numbers").split()
+p,q = [int(x) for x in input("Enter two value: ").split()]
+print(x,y,d)
+print(p,q):wq
